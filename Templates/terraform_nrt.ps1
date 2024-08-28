c118027981904b156bcb75948f6bcc6f5bd13739
bittib010 <skaftunen@gmail.com>
2024-08-29 00:03:44 +0200

    fix: NRT link commented out

--- Templates/terraform_nrt.ps1
+++ Templates/terraform_nrt.ps1
@@ -1,7 +1,7 @@
 # Main Template starts here:
 @"
 resource "azurerm_sentinel_alert_rule_nrt" "nrt_$guid" {
-  https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/sentinel_alert_rule_nrt
+  // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/sentinel_alert_rule_nrt
   name                       = "$guid"
   log_analytics_workspace_id = var.log_analytics_workspace_id
   display_name               = "$($row.Name)"
